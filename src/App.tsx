4b52eb6f0ed496410cae6883a24ed0941272532c
prgmr99 <johnyeom24@naver.com>
2023-01-11 15:06:41 +0900

    clean push

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,79 @@
-import React from 'react';
-import { useState } from 'react';
-import Circle from './Circle';
-import styled from 'styled-components';
+import { createGlobalStyle } from 'styled-components';
+import Router from './Router';
 
-const Container = styled.div`
-  background-color: ${props => props.theme.bgColor};
+// 스타일 컴포넌트는 고립되어 있어.
+// GlobalStyle은 한 컴포넌트를 만들 수 있게 해주는데 
+// 렌더링될 때, 그 컴포넌트는 전역 스코프에 스타일을 올려줘.
+// 브라우저의 기본값들을 초기화하는 방법
+const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Raleway:wght@200;400&display=swap');
+  html, body, div, span, applet, object, iframe,
+  h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+  a, abbr, acronym, address, big, cite, code,
+  del, dfn, em, img, ins, kbd, q, s, samp,
+  small, strike, strong, sub, sup, tt, var,
+  b, u, i, center,
+  dl, dt, dd, menu, ol, ul, li,
+  fieldset, form, label, legend,
+  table, caption, tbody, tfoot, thead, tr, th, td,
+  article, aside, canvas, details, embed,
+  figure, figcaption, footer, header, hgroup,
+  main, menu, nav, output, ruby, section, summary,
+  time, mark, audio, video {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font-size: 100%;
+    font: inherit;
+    vertical-align: baseline;
+  }
+  /* HTML5 display-role reset for older browsers */
+  article, aside, details, figcaption, figure,
+  footer, header, hgroup, main, menu, nav, section {
+    display: block;
+  }
+  /* HTML5 hidden-attribute fix for newer browsers */
+  *[hidden] {
+      display: none;
+  }
+  body {
+    line-height: 1;
+  }
+  menu, ol, ul {
+    list-style: none;
+  }
+  blockquote, q {
+    quotes: none;
+  }
+  blockquote:before, blockquote:after,
+  q:before, q:after {
+    content: '';
+    content: none;
+  }
+  table {
+    border-collapse: collapse;
+    border-spacing: 0;
+  }
+  * {
+    box-sizing: border-box;
+  }
+  body {
+    font-family: 'Raleway', sans-serif;
+    background-color: ${props => props.theme.bgColor};
+    color: ${props => props.theme.textColor};
+  }
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
 `;
-const H1 = styled.h1`
-  color: ${props => props.theme.textColor};
-`; 
 
 function App() {
-
   return (
-    <div>
-      <Container>
-        <H1>protected</H1>
-      </Container>
-    </div>
+    <>
+      <GlobalStyle/> 
+      <Router/>
+    </>
   );
 }
 
